1e7d08f5b24f0e0e10811cd78f185b75dd6b44ed
Yash Goswami <145782019+Yashgoswami41@users.noreply.github.com>
2026-05-21 12:31:51 +0530

    Add login page functionality and test case for HRM application

---
+++ Playwright_1/Pages/login_hrm_pages.py
@@ -0,0 +1,8 @@
+from playwright.sync_api import Page
+
+class loginpage:
+    def __init__(self, page: Page):
+        self.page = page
+        self.username_input = page.get_by_role("textbox", name="Username")
+        self.password_input = page.get_by_role("textbox", name="Password")
+        self.login_button = page.get_by_role("button", name="Login")
