be40ad9cb43254ddf727d61a6485d841d503fe6d
Yash Goswami <145782019+Yashgoswami41@users.noreply.github.com>
2026-05-22 23:02:06 +0530

    Implement login functionality and refactor test case for HRM application

---
+++ Playwright_1/Pages/hrm_home_page.py
@@ -0,0 +1,17 @@
+from playwright.sync_api import page, expect
+
+class home_page:
+    def __init__(self, page: page):
+        self.page = page
+        self.upgrade_btn = page.get_by_role("button", name="Upgrade")
+        self.performance_link = page.get_by_role("link", name="Performance")
+        self.dashboard_link = page.get_by_role("link", name="Dashboard")
+
+    def is_upgrade_button_visible(self):
+        return self.upgrade_btn.is_visible()
+    
+    def click_performance(self):
+        return self.performance_link.click()
+    
+    def click_dashboard(self):
+        self.dashboard_link.click()
--- Playwright_1/tests/test_login_hrm.py
+++ Playwright_1/tests/test_login_hrm.py
@@ -1,16 +1,20 @@
 import re
 from playwright.sync_api import Page, expect
+from Pages.login_hrm_pages import loginpage
+from Pages.hrm_home_page import home_page
 
 
 def test_example(page: Page) -> None:
     page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    page.get_by_role("textbox", name="Username").click()
-    page.get_by_role("textbox", name="Username").press("CapsLock")
-    page.get_by_role("textbox", name="Username").fill("A")
-    page.get_by_role("textbox", name="Username").press("CapsLock")
-    page.get_by_role("textbox", name="Username").fill("Admin")
-    page.get_by_role("textbox", name="Password").click()
-    page.get_by_role("textbox", name="Password").fill("admin123")
-    page.get_by_role("button", name="Login").click()
-    expect(page.get_by_role("button", name="Upgrade")).to_be_visible()
-    page.get_by_role("link", name="Performance").click()
+    login = loginpage(page)
+    login.login("Admin", "admin123")
+    home = home_page(page)
+
+    login_page.enter_username("Admin")
+    login_page.enter_password("admin123")
+    login_page.click_login() 
+
+    expect(home.is_upgrade_button_visible()).to_be_true()
+    home.click_performance()
+    home.click_dashboard()
+    
